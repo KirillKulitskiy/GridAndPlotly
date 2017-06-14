33ccac67822c3fe82e02168cc4c1960e1b68672b
KKulitskiy-PC\KKulitskiy <frantichero@gmail.com>
2017-06-14 13:46:40 +0300

    Implemented row highlighting on the same conditions.

--- scripts/app.js
+++ scripts/app.js
@@ -12,6 +12,34 @@ app.controller('mainController', ['$scope', 'uiGridConstants', function($scope, 
             3: $scope.y[2],
             4: $scope.y[3],
             5: $scope.y[4]
+        },
+        {
+            1: $scope.y[0],
+            2: $scope.y[1],
+            3: $scope.y[2],
+            4: $scope.y[3],
+            5: $scope.y[4]
+        },
+        {
+            1: $scope.y[0],
+            2: $scope.y[1],
+            3: $scope.y[2],
+            4: $scope.y[3],
+            5: $scope.y[4]
+        },
+        {
+            1: $scope.y[0],
+            2: $scope.y[1],
+            3: $scope.y[2],
+            4: $scope.y[3],
+            5: $scope.y[4]
+        },
+        {
+            1: $scope.y[0],
+            2: $scope.y[1],
+            3: $scope.y[2],
+            4: $scope.y[3],
+            5: $scope.y[4]
         }];
 
     $scope.gridOptions = {
@@ -42,7 +70,12 @@ app.controller('mainController', ['$scope', 'uiGridConstants', function($scope, 
         margin: { t: 0 } } );
     TESTER.on('plotly_click', function(data){
         for(var i=0; i < $scope.gridOptions.columnDefs.length; i++) {
-            $scope.gridOptions.columnDefs[i].field == data.points[0].x ? $scope.gridOptions.columnDefs[i].cellClass = 'blue' : $scope.gridOptions.columnDefs[i].cellClass = 'white';
+            $scope.gridOptions.columnDefs[i].cellClass = function(grid, row, col, rowRenderIndex, i) {
+                if (rowRenderIndex == data.points[0].x - 1) {
+                    return 'blue';
+                }
+                return 'white';
+            }
         }
 
         $scope.gridApi.core.notifyDataChange( uiGridConstants.dataChange.COLUMN );
